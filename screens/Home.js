385471d709e48fbfd13c5c127715406d06a704ad
Aashiq Ahmed M <ahmed.aashiq@saama.com>
2022-10-20 13:07:02 +0530

    added a button to go to ReviewDetails page

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,16 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
 import { globalStyles } from '../styles/global.js'
 
-function Home() {
+function Home(props) {
+	const pressHandler = () => {
+		props.navigation.navigate('ReviewDetails')
+	}
 	return (
 		<View style={globalStyles.container}>
 			<Text style={globalStyles.titleText}>Home Screen</Text>
+			<Button title='Go to Review' onPress={pressHandler} />
 		</View>
 	)
 }
-
 
 export default Home
